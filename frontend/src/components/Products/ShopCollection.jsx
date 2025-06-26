c0b7238226b8a083f5b184d708a7502423868303
rawfile <rawfile.webdev@gmail.com>
2025-06-26 17:51:17 +0100

    Shop collection page added

---
+++ frontend/src/components/Products/ShopCollection.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import livingRoom from "../../assets/shop-collection.png";
+import bedroom from "../../assets/shop-collection2.png";
+import kitchen from "../../assets/shop-collection3.png";
+import { Link } from "react-router-dom";
+import { BsArrowRight } from "react-icons/bs";
+
+const ShopCollection = () => {
+  return (
+    <div className="container mx-auto px-2 mt-6 ">
+      {/* Top Head */}
+      <div className="md:flex justify-between items-center">
+        <h1 className="text-4xl md:text-5xl font-semibold w-full mb-5 tracking-wide">
+          Simply Unique <span className="text-gray-500">/</span> <br />
+          Simply Better <span className="text-gray-500">.</span>
+        </h1>
+        <p className="text-gray-600">
+          {" "}
+          <span className="font-semibold text-black">3legant</span> is a gift &
+          decorations store based in HCMC, Vietnam. Est since 2019.{" "}
+        </p>
+      </div>
+
+      {/* Low Head */}
+      <div className="grid grid-cols-1 gap-4 mt-10 md:grid-cols-4 md:grid-rows-2">
+        {/* Living Room */}
+        <div className="md:col-span-2 md:row-span-2 relative">
+          <div className="absolute lg:left-14 lg:top-20 md:left-10 md:top-10 lg:space-y-2 left-14 top-16 md:space-y-0">
+            <h1 className="lg:text-3xl md:text-xl font-semibold text-2xl">Living Room</h1>
+            <Link to="/collections" className="inline-flex items-center gap-2 text-lg hover:text-blue-500 transition duration-300 border-b-2 border-b-black hover:border-b-blue-500 lg:text-xl">Shop Now
+            <BsArrowRight className='h-4 w-4 md:h-6 md:w-6 ' />
+            </Link>
+          </div>
+          <img
+            src={livingRoom}
+            alt="Living Room"
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        {/* Bedroom */}
+        <div className="md:col-span-2 relative">
+          <div className="absolute lg:left-14 lg:bottom-20 md:left-8 md:bottom-10 left-14 bottom-20 lg:space-y-2 md:space-y-0">
+            <h1 className="lg:text-3xl md:text-xl font-semibold text-2xl">Bed Room</h1>
+            <Link to="/collections" className="inline-flex items-center gap-2 text-lg hover:text-blue-500 transition duration-300 border-b-2 border-b-black hover:border-b-blue-500 lg:text-xl">Shop Now
+            <BsArrowRight className='h-4 w-4 md:h-6 md:w-6 ' />
+            </Link>
+          </div>
+          <img
+            src={bedroom}
+            alt="Bedroom"
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        {/* Kitchen */}
+        <div className="md:col-span-2 relative">
+          <div className="absolute lg:left-14 lg:bottom-20 md:left-8 md:bottom-10 left-14 bottom-20 lg:space-y-2 md:space-y-0">
+            <h1 className="lg:text-3xl md:text-xl font-semibold text-2xl">Kitchen</h1>
+            <Link to="/collections" className="inline-flex items-center gap-2 text-lg hover:text-blue-500 transition duration-300 border-b-2 border-b-black hover:border-b-blue-500 lg:text-xl">Shop Now
+            <BsArrowRight className='h-4 w-4 md:h-6 md:w-6 ' />
+            </Link>
+          </div>
+          <img
+            src={kitchen}
+            alt="Kitchen"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShopCollection;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Hero from '../components/Layout/Hero';
+import ShopCollection from '../components/Products/ShopCollection';
 
 const Home = () => {
   return <div>
     <Hero />
+    <ShopCollection />
   </div>;
 };
 
