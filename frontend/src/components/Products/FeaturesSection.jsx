6a4d62bb00d457a3b7afb86c9aa874113c46cef0
rawfile <rawfile.webdev@gmail.com>
2025-06-28 16:15:06 +0100

    Modified already made sections

--- frontend/src/components/Products/FeaturesSection.jsx
+++ frontend/src/components/Products/FeaturesSection.jsx
@@ -8,35 +8,35 @@ const FeaturesSection = () => {
     <section className=" mt-10">
       <div className="container mx-auto p-6">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-          <div className="bg-sec w-full h-64 p-10 flex flex-col gap-4 items-start justify-center">
-            <MdLocalShipping className="text-4xl" />
+          <div className="bg-sec w-full h-64 p-10 flex flex-col gap-4 items-start justify-center hover:bg-blue-500 transition-all duration-500 group">
+            <MdLocalShipping className="text-4xl group-hover:text-white transition-all duration-300" />
             <div>
-              <h3 className="font-semibold md:text-xl">Free Shipping</h3>
-              <p className="text-gray-500 md:text-lg">Orders over $200</p>
+              <h3 className="font-semibold md:text-xl group-hover:text-white transition-all duration-300">Free Shipping</h3>
+              <p className="text-gray-500 md:text-lg group-hover:text-white transition-all duration-300">Orders over $200</p>
             </div>
           </div>
 
-          <div className="bg-sec w-full h-64 p-10 flex flex-col gap-4 items-start justify-center">
-            <FaMoneyCheckAlt className="text-4xl " />
+          <div className="bg-sec w-full h-64 p-10 flex flex-col gap-4 items-start justify-center group hover:bg-blue-500 transition duration-500">
+            <FaMoneyCheckAlt  className="text-4xl group-hover:text-white transition-all duration-300" />
             <div>
-              <h3 className="font-semibold md:text-xl">Money Back</h3>
-              <p className="text-gray-500 md:text-lg">30 days guarantee</p>
+              <h3 className="font-semibold md:text-xl group-hover:text-white transition-all duration-300">Money Back</h3>
+              <p className="text-gray-500 md:text-lg group-hover:text-white transition-all duration-300">30 days guarantee</p>
             </div>
           </div>
 
-          <div className="bg-sec w-full h-64 p-10 flex flex-col gap-4 items-start justify-center">
-            <MdLock className="text-4xl " />
+          <div className="bg-sec w-full h-64 p-10 flex flex-col gap-4 items-start justify-center group hover:bg-blue-500 transition duration-500">
+            <MdLock  className="text-4xl group-hover:text-white transition-all duration-300" />
             <div>
-              <h3 className="font-semibold md:text-xl">Secure Payment</h3>
-              <p className="text-gray-500 md:text-lg">Secure by Stripe</p>
+              <h3 className="font-semibold md:text-xl group-hover:text-white transition-all duration-300">Secure Payment</h3>
+              <p className="text-gray-500 md:text-lg group-hover:text-white transition-all duration-300">Secure by Stripe</p>
             </div>
           </div>
 
-          <div className="bg-sec w-full h-64 p-10 flex flex-col gap-4 items-start justify-center">
-            <FaPhoneVolume className="text-4xl -rotate-45" />
+          <div className="bg-sec w-full h-64 p-10 flex flex-col gap-4 items-start justify-center group hover:bg-blue-500 transition duration-500">
+            <FaPhoneVolume className="text-4xl group-hover:text-white transition-all duration-300 -rotate-45" />
             <div>
-              <h3 className="font-semibold md:text-xl">24/7 Support</h3>
-              <p className="text-gray-500 md:text-lg">Online 24 hours</p>
+              <h3 className="font-semibold md:text-xl group-hover:text-white transition-all duration-300">24/7 Support</h3>
+              <p className="text-gray-500 md:text-lg group-hover:text-white transition-all duration-300">Online 24 hours</p>
             </div>
           </div>
         </div>
